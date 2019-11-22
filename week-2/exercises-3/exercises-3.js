58dbea0c3b2eeb0d551d836a3f68b1a3f71eb0db
Muhammad Anova <anovanurfaqih@gmail.com>
2019-11-23 05:28:38 +0700

    exercises-3

--- week-2/exercises-3/exercises-3.js
+++ week-2/exercises-3/exercises-3.js
@@ -1,13 +1,13 @@
 const game = 'Roket Terbang'
 // bebas memilih nama
-var nama = 'Zero'
-var peran = 'Penyihir'
+var nama = 'Danu'
+var peran = 'Tendang'
 
 // action dari peran
 var action1 = 'kamu dapat menyerang dengan senjatamu!'
 var action2 = 'kamu akan membantu temanmu yang terluka.'
 var action3 = 'ciptakan keajaiban yang membantu kemenanganmu!'
-var action4 = 'peran kamu anak bawang'
+var action4 = 'silahkan isi peran (Ksatria/Tabib/Penyihir)'
 
 // pesan selamat datang
 var welcome = `Selamat datang di Dunia ${game}, ${nama}`
@@ -20,23 +20,26 @@ if(nama !== '' && peran === ''){
   // Penggunaan string + variabel
   console.log('Halo ' + peran + ' ' + nama + ', ' + action1)
   // Penggunaan backticks
-  console.log(`Halo ${peran} ${nama}, ${action1}`)
+  // console.log(`Halo ${peran} ${nama}, ${action1}`)
 }else if(nama === 'Danu' && peran === 'Tabib'){
   console.log(welcome)
   console.log('Halo ' + peran + ' ' + nama + ', ' + action2)
 }else if(nama === 'Zero' && peran === 'Penyihir'){
   console.log(welcome)
   console.log('Halo ' + peran + ' ' + nama + ', ' + action3)
+}else if(nama !== '' && peran !== ''){
+  console.log(welcome)
+  console.log('Halo' + ' ' + nama + ', ' + action4 )
 }else{
   console.log('Nama harus diisi')
 }
 
 // coba kondisi dengan binding nama peran yang dikhususkan
-if(nama !== '' && peran === ''){
-  console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`)
-}else if(nama !== '' && peran !== ''){
-  console.log(`${welcome}`)
-  console.log(`Halo ${peran} ${nama}, ${peran === 'Ksatria' ? action1 : peran === 'Tabib' ? action2 : peran === 'Penyihir' ? action3 : action4}`)
-}else{
-  console.log('Nama harus diisi')
-}
+// if(nama !== '' && peran === ''){
+//   console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`)
+// }else if(nama !== '' && peran !== ''){
+//   console.log(`${welcome}`)
+//   console.log(`Halo ${peran} ${nama}, ${peran === 'Ksatria' ? action1 : peran === 'Tabib' ? action2 : peran === 'Penyihir' ? action3 : action4}`)
+// }else{
+//   console.log('Nama harus diisi')
+// }
